ca17ae4c52731aacf8eac8232d35d7e12e0ac121
Prashant Kumar <prashant.aman9@gmail.com>
2024-08-08 14:09:44 +0530

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
+import React from 'react'
+import './../styles/App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ItemList from '../pages/ItemList'
+import ItemDetail from '../pages/ItemDetail'
 
-import React from "react";
-import './../styles/App.css';
+const items = [
+	{
+		id: 1,
+		name: 'Item 1',
+		description: 'Description for Item 1',
+	},
+	{
+		id: 2,
+		name: 'Item 2',
+		description: 'Description for Item 2',
+	},
+	{
+		id: 3,
+		name: 'Item 3',
+		description: 'Description for Item 3',
+	},
+]
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+  return <div>{/* Do not remove the main div */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ItemList items={items} />} />
+        <Route path="/items/:id" element={<ItemDetail items={items}/>} />
+      </Routes>
+    </BrowserRouter>
+  </div>
 }
 
 export default App
